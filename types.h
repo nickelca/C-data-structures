07eb24212957f3b66932a1b7d1d91f6662aac3ef
Chase Nickel <nickel5@outlook.com>
2024-11-27 22:33:57 -0600

    Project setup

---
+++ types.h
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdint.h>
+
+typedef size_t usize;
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef unsigned long long ullong;
+typedef long long llong;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
